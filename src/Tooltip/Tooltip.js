f6d5717229b8c8718a3b2ab81990d1151e38c3d1
lane <laneolson@gmail.com>
2021-09-03 11:46:37 -0700

    feat: populate tooltip with data

--- src/Tooltip/Tooltip.js
+++ src/Tooltip/Tooltip.js
@@ -1,6 +1,10 @@
-import React from "react";
-import { withStyles } from "@material-ui/core";
+import React, { useRef } from "react";
+import { Box, Divider, Paper, Typography, withStyles } from "@material-ui/core";
 import { useTooltipData } from ".";
+import clsx from "clsx";
+import { TOOLTIP_METRICS } from "../Dashboard/constants";
+import { useLang } from "../Language";
+import { useFormatters } from "../Dashboard/hooks/useFormatter";
 
 const styles = (theme) => ({
   root: {
@@ -11,13 +15,49 @@ const styles = (theme) => ({
   },
 });
 
+const METRIC_KEYS = TOOLTIP_METRICS.map((m) => `METRIC_${m}`);
+
+const getTooltipTitle = (data) => {
+  if (!data || !data.name) return ["Unavailable", ""];
+  const [name, parent] = data.name.split(",");
+  return [name, parent || "Texas"];
+};
+
 const Tooltip = ({ classes, ...props }) => {
   const data = useTooltipData();
-  return data ? (
-    <pre className={classes.root} {...props}>
-      {JSON.stringify(data, null, 2)}
-    </pre>
-  ) : null;
+  // keep a ref to the data so we can gracefull fade out tooltip
+  const dataRef = useRef(null);
+  const rowLabels = useLang(METRIC_KEYS);
+  const rowFormatters = useFormatters(TOOLTIP_METRICS);
+  // update the data reference when the data changes
+  if (data) dataRef.current = data;
+
+  if (!dataRef.current) return null;
+  const [name, parent] = getTooltipTitle(dataRef.current);
+  const rowData = TOOLTIP_METRICS.map((m, i) =>
+    rowFormatters[i](dataRef.current[m])
+  );
+  return (
+    <Paper className={clsx(classes.root)}>
+      <Box p={2}>
+        <Typography variant="body1">{name}</Typography>
+        {parent && <Typography variant="body2">{parent}</Typography>}
+      </Box>
+      <Divider />
+      {rowLabels.map((label, i) => (
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          key={i}
+          py={1}
+          px={2}
+        >
+          <Typography variant="body2">{label}</Typography>
+          <Typography variant="body2">{rowData[i]}</Typography>
+        </Box>
+      ))}
+    </Paper>
+  );
 };
 
 export default withStyles(styles)(Tooltip);
