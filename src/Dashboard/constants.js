598575d291228e70d422f53844a8c752abde7f2f
lane <laneolson@gmail.com>
2021-09-27 09:02:21 -0700

    fix: add renter households to tooltip

--- src/Dashboard/constants.js
+++ src/Dashboard/constants.js
@@ -5,4 +5,4 @@ export const DEFAULT_BUBBLE_COLOR = "#e98816";
 export const EVICTION_DATA_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 // metrics to show in the tooltip (TODO: move to config props)
-export const TOOLTIP_METRICS = ["ef", "efr", "mfa", "tfa"];
+export const TOOLTIP_METRICS = ["ef", "efr", "pop", "mfa", "tfa"];
