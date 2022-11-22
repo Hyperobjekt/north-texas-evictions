5a951a1663faff1378145657164035c33c1ccff4
Chris Hull <chris.f.hull@gmail.com>
2022-11-22 09:31:02 -0500

    Update Dashboard.js

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -69,7 +69,7 @@ const Dashboard = ({ config, ...props }) => {
               transform: "translate(-50%, -50%)",
               width: 400,
               bgcolor: "background.paper",
-              border: "2px solid #000",
+              borderRadius: 4,
               boxShadow: 24,
               p: 4,
               display: "flex",
